aabd4fd69762ab3f0dfed660b14a949014ee815e
Malcom <gtshilombowanticale@adneom.com>
2017-10-02 12:26:53 +0200

    first commit

---
+++ app/src/main/java/com/test/gradle_poc/MainActivityGradle.java
@@ -0,0 +1,13 @@
+package com.test.gradle_poc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityGradle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_gradle);
+    }
+}
